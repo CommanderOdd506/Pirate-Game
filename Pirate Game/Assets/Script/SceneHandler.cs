a05e630bf882cb4c2b0a9119f02ad34b72b7a8a0
drewp03 <drewprato03@gmail.com>
2026-04-06 20:53:03 -0400

    Menus Added + Colliders for Map

--- Pirate Game/Assets/Script/SceneHandler.cs
+++ Pirate Game/Assets/Script/SceneHandler.cs
@@ -5,35 +5,107 @@ using UnityEngine.SceneManagement;
 
 public class SceneHandler : MonoBehaviour
 {
+	public PlayerInput playerInput;
+	private bool isPaused = false;
+	public GameObject returnButton;
+
+	private void Start()
+	{
+		returnButton.SetActive(false);
+	}
+
+	private void Update()
+	{
+		if(playerInput.pausePressed)
+		{
+			if(isPaused)
+				Resume();
+
+			else
+				Pause();
+		}
+	}
+
+	private void Pause()
+	{
+		Time.timeScale = 0f;
+		isPaused = true;
+
+		returnButton.SetActive(true);
+
+		Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+	}
+
+	private void Resume()
+	{
+		Time.timeScale = 1;
+		isPaused = false;
+
+		returnButton.SetActive(false);
+
+		Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+	}
+
 	public void HubWorld()
 	{
 		SceneManager.LoadSceneAsync(0);
+
+		Resume();
 	}
 
 	public void LevelOne()
 	{
 		SceneManager.LoadSceneAsync(1);
-		Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+
+		Resume();
 	}
 
 	public void LevelTwo()
 	{
 		SceneManager.LoadSceneAsync(2);
-		Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+
+		Resume();
 	}
 
 	public void LevelThree()
 	{
 		SceneManager.LoadSceneAsync(3);
-		Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
+
+		Resume();
 	}
 
 	public void MapWorld()
 	{
 		SceneManager.LoadSceneAsync(4);
+
+		Resume();
+	}
+
+	public void Settings()
+	{
+		SceneManager.LoadSceneAsync(5);
+
+		Resume();
+
+		Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+	}
+
+	public void MainMenu()
+	{
+		SceneManager.LoadSceneAsync(6);
+
+		Resume();
+
+		Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
 	}
 
 }
