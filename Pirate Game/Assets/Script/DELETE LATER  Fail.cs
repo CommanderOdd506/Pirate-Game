035f59fa9c618bd00d72e0c515973674aaee85bd
jeshalonsb <jeshalonsb@gmail.com>
2026-04-08 13:13:33 -0400

    Fort Additions
    Respawn checkpoints added to the level

--- Pirate Game/Assets/Script/DELETE LATER  Fail.cs
+++ Pirate Game/Assets/Script/DELETE LATER  Fail.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class DeletelaterFailScript : MonoBehaviour
 {
-    public Transform respawn;
-    public GameObject player;
-    
+    public Transform respawnOne;
+    public Transform respawnTwo;
+    public Transform respawnThree;
+
+    public static Transform currentRespawn;
+
+    void Awake()
+    {
+        if (currentRespawn == null)
+        {
+            currentRespawn = respawnOne;
+            Debug.Log("Default respawn set to respawnOne");
+        }
+    }
+
+    public void SetRespawnPoint(int index)
+    {
+        if (index == 1) currentRespawn = respawnOne;
+        if (index == 2) currentRespawn = respawnTwo;
+        if (index == 3) currentRespawn = respawnThree;
+
+        Debug.Log("Respawn updated to: " + currentRespawn.name);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Rigidbody rb = other.GetComponent<Rigidbody>();
+            Debug.Log("Respawning player at: " + currentRespawn.name);
 
-            if (rb != null )
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
             {
                 rb.velocity = Vector3.zero;
                 rb.angularVelocity = Vector3.zero;
@@ -23,14 +45,15 @@ public class DeletelaterFailScript : MonoBehaviour
             if (cc != null)
             {
                 cc.enabled = false;
-                other.transform.position = respawn.position;
+                other.transform.position = currentRespawn.position;
                 cc.enabled = true;
             }
             else
             {
-                other.transform.position = respawn.position;
+                other.transform.position = currentRespawn.position;
             }
-            other.transform.rotation = respawn.rotation;
+
+            other.transform.rotation = currentRespawn.rotation;
         }
     }
 }
