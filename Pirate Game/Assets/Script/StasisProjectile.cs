6f61a09d02e2a52c8053fd66458537a49d80d218
CommanderOdd506 <155345740+CommanderOdd506@users.noreply.github.com>
2026-04-18 02:23:23 -0400

    Checkpoint system

--- Pirate Game/Assets/Script/StasisProjectile.cs
+++ Pirate Game/Assets/Script/StasisProjectile.cs
@@ -5,40 +5,37 @@ using UnityEngine;
 public class StasisProjectile : MonoBehaviour, IStasisable
 {
     [SerializeField] private float speed;
-
     [SerializeField] Renderer rend;
     [SerializeField] Material normalMat;
     [SerializeField] Material highlightMat;
 
     private bool isStasised = false;
 
+    public event System.Action<IStasisable> OnDestroyed;
 
     void Update()
     {
-        if(!isStasised)
-        {
+        if (!isStasised)
             transform.position += transform.forward * speed * Time.deltaTime;
-        }
-
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (!isStasised)
-        {
-            if (collision.gameObject.CompareTag("Player"))
-            {
-                //TO-DO Kill Player;
-            }
-            Destroy(this.gameObject);
-        }
+        if (isStasised) return;
+
+        if (collision.gameObject.CompareTag("Player"))
+            CheckpointManager.Instance.RespawnPlayer();
+
+        Destroy(gameObject);
     }
 
-    //stasis Implementation;
+    void OnDestroy()
+    {
+        OnDestroyed?.Invoke(this);
+    }
 
     public void BeginStasis() => isStasised = true;
     public void EndStasis() => isStasised = false;
-
     public void OnStasisTargeted() => rend.material = highlightMat;
     public void OnStasisUntargeted() => rend.material = normalMat;
 }
