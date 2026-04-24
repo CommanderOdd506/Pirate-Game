7337b37fbf1d5eb4c4634f025a403439d2ceab12
CommanderOdd506 <155345740+CommanderOdd506@users.noreply.github.com>
2026-04-24 16:37:28 -0400

    Collectible and hub changes

--- Pirate Game/Assets/Script/Collectible.cs
+++ Pirate Game/Assets/Script/Collectible.cs
@@ -12,6 +12,6 @@ public class Collectible : MonoBehaviour
         if (!other.CompareTag("Player")) return;
 
         CollectibleSystem.Instance.Add(id, value);
-        Destroy(gameObject);
+        this.gameObject.SetActive(false);
     }
 }
