bf16454d1e064685aa6962b260410fcb9a1b9af3
drewp03 <drewprato03@gmail.com>
2026-05-02 19:52:01 -0400

    Controls Added To Settings and Main Menu

--- Pirate Game/Assets/Script/MainMenu.cs
+++ Pirate Game/Assets/Script/MainMenu.cs
@@ -7,11 +7,41 @@ using UnityEngine.InputSystem;
 public class MainMenu : MonoBehaviour
 {
     public GameObject firstButtonOnMainPage;
+
+    public GameObject gameImage;
+
+    public GameObject controlsPage;
+    public GameObject MainPage;
+
+    public GameObject returnButton;
+
     // Start is called before the first frame update
     void Start()
     {
         EventSystem.current.SetSelectedGameObject(firstButtonOnMainPage);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+    }
+
+    public void ControlsButton()
+    {
+        MainPage.SetActive(false);
+
+        gameImage.SetActive(false);
+
+        controlsPage.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(returnButton);
+    }
+
+    public void ReturnToMain()
+    {
+        controlsPage.SetActive(false);
+
+        gameImage.SetActive(true);
+
+        MainPage.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(firstButtonOnMainPage);
     }
 }
